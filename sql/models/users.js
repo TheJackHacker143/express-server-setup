a4e14605dd82b4085418ea4cce59af3fca734c30
jagannath parida <dadajagu876@gmail.com>
2026-01-06 21:44:03 +0530

    hii

--- sql/models/users.js
+++ sql/models/users.js
@@ -64,26 +64,79 @@ const reteriveEntry=async (req,res)=>{
  module.exports={addentries,updateEntry,deleteEntry,reteriveEntry}
 
 
-// const db=require("../utils/dbConnection")
-// const addentries=(req,res)=>{
-//     const {email,name}=req.body
-// const insertQuery=`insert into users (email,name) values(?,?)`
-// db.query(insertQuery,[email,name],(err)=>{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // const addentries=(req,res)=>{
+//     const {email,name,age}=req.body
+// const insertQuery=`insert into students (email,name,age) values(?,?,?)`
+// db.query(insertQuery,[email,name,age],(err)=>{
 //     if(err){
 //         console.log(err);
 //         res.status(500).send(err.message)
-//         //db.end();
+//         db.end();
 //         return;
 //     }
 //     console.log("value inserted")
 //     res.status(200).send(`students with name ${name} added`)
 // })
 // }
+// const updateEntry=(req,res)=>{
+//     const id=req.params.id
+//     const {name}=req.body
+//     console.log("PARAM ID:", id);
+// console.log("BODY NAME:", name);
+
+//     const updateQuery="update students set name= ? where id= ?";
+//     db.query(updateQuery,[name,id],(err,result)=>{
+//         if(err){
+//             console.log(err.message)
+//             res.status(500).send(err.message)
+//             //db.end();
+//             return;
+//         }
+//         if(result.affectedRows===0){
+//             console.log("updated not succssfully")
+            
+//             res.status(400).send("student not found")
+// return;
+//         }
+//         res.status(200).send("student  updated")
+
+//             console.log("updated succssfully")
+// })
+// }
+// const deleteEntry=(req,res)=>{
+//     const id=req.params.id
+//     const deleteQuery=`delete from students where id=?`
+//     db.query(deleteQuery,[id],(err,result)=>{
+// res.send("hello")
+//     })
+// }
 // const reteriveEntry=(req,res)=>{
-//     db.query("SELECT * FROM users", (err, result) => {
+//     db.query("SELECT * FROM students", (err, result) => {
 //     if (err) return res.status(500).send("DB error");
 //     console.log(result)
 //     res.send(result); // ✅ yahan actual DB data hota hai
 //   });
 // }
-// module.exports={addentries,reteriveEntry}
+// module.exports={addentries,updateEntry,deleteEntry,reteriveEntry}
