20fe1f5423458471c6bddc30967108da4ca0ab89
jagannath parida <dadajagu876@gmail.com>
2026-03-04 17:56:42 +0530

    commoited

--- expenseBackend/models/ForgotPasswordRequests.js
+++ expenseBackend/models/ForgotPasswordRequests.js
@@ -1,14 +1,33 @@
-const{DataTypes}=require("sequelize");
-const { v4: uuidv4 } = require('uuid')
-const sequelize=require("../dbconnection")
-const ForgotPasswordRequests=sequelize.define('ForgotPasswordRequests',{
-    id:{
-        type:DataTypes.UUID,
-        primaryKey:true,
-        defaultValue:uuidv4,
-        allowNull:false},
-    isactive:DataTypes.BOOLEAN
+const { DataTypes } = require("sequelize");
+const sequelize = require("../dbconnection"); // your sequelize instance
 
-})
-console.log("users table created")
-module.exports=ForgotPasswordRequests;
+const ForgotPasswordRequests = sequelize.define('ForgotPasswordRequests', {
+    id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4, // Automatic random UUID
+        allowNull: false
+    },
+    isactive: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true, // reset link active by default
+        allowNull: false
+    },
+    expiresAt: {
+        type: DataTypes.DATE,
+        allowNull: false // will set expiry when creating a new reset request
+    },
+    userId: {
+        type: DataTypes.INTEGER, // Assuming Users table id is INTEGER
+        allowNull: false
+    }
+}, {
+    timestamps: true,   // createdAt and updatedAt automatically
+    tableName: 'ForgotPasswordRequests'
+});
+
+console.log("ForgotPasswordRequests table ready / synced");
+
+console.log("ForgotPasswordRequests table ready / synced");
+
+module.exports = ForgotPasswordRequests;
